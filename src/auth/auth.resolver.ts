721ba2018eb64a99a66880a1a4d8684e198d0066
davferod <davidrd81@gmail.com>
2026-05-30 10:44:14 -0500

    fix(auth): remove SingupResponse duplicate, unify signup return type to LoginResponse, remove unused SignupInput DTO

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -5,7 +5,6 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LoginInput } from './dto';
 import { AuthService } from './auth.service';
 import { LoginResponse} from './dto/login-response';
-import { SingupResponse } from './dto/singup-response';
 import { LoginUserInput } from './dto/login-user.input';
 import { User } from 'src/users/entities/user.entity';
 import { CurrentUser } from './decorators/current-user.decorator';
@@ -25,7 +24,7 @@ export class AuthResolver {
         return this.authService.login(loginInput);
     }
 
-    @Mutation(() => SingupResponse, { name: 'signup'})
+    @Mutation(() => LoginResponse, { name: 'signup'})
     async signup(
         @Args('loginUserInput') loginUserInput: LoginUserInput): Promise<LoginResponse> {
         return this.authService.signup(loginUserInput);
